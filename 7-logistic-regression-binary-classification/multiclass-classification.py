4702451ab1f01a0fb4b61b1ab968c578163276e0
nsatyaprakash <n.satyaprakash@yahoo.com>
2024-06-27 14:12:09 +0530

    Saving changes

--- 7-logistic-regression-binary-classification/multiclass-classification.py
+++ 7-logistic-regression-binary-classification/multiclass-classification.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
 
 digits = load_digits()
 print(dir(digits))
@@ -21,11 +23,20 @@ reg = LogisticRegression(max_iter=1500)
 reg.fit(X_train, y_train)
 
 print("Model score = ",reg.score(X_test, y_test))
-
-print("Model prediction on test data = ", reg.predict(X_test))
+y_predicted = reg.predict(X_test)
+print("Model prediction on test data = ", y_predicted)
 
 plt.matshow(digits.images[108])
 plt.show()
 
 print("Image at index 108 is = ", digits.target[108])
 print("Image prediction result is = ", reg.predict([digits.data[108]]))
+
+"""Confusion matrix"""
+cm = confusion_matrix(y_test, y_predicted)
+
+plt.figure(figsize=(10, 7))
+sn.heatmap(cm, annot=True)
+plt.xlabel('Predicted')
+plt.ylabel('Truth')
+plt.show()
